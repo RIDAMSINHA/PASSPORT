5277456b450eb179bf071e27f52f132cba31872d
RIDAMSINHA <ridamsinha20@gmailcom>
2024-01-27 11:38:46 +0530

    new commit

--- src/components/Sign_up.js
+++ src/components/Sign_up.js
@@ -1,5 +1,321 @@
 import React from 'react';
 import '../styles/style.css';
+import { ethers } from 'ethers';
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const _token = event.target.aadhar.value;
+  const _rname = event.target.fullname.value;
+  const _pcd = event.target.address.value;
+  const _password = event.target.password.value;
+  const _uuid = event.target.email.value;
+
+
+  const govContractAddress = '0xCaA4C7A54D8506F33EDe1FFEc985D2FAA8cba26e';
+  const GOV_CONTRACT_BYTECODE = '';
+  const GOV_CONTRACT_ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_transportName",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_location",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "deployBorderAuthorityContract",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_rname",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_pcd",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_password",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "deploySubContract",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_country",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_location",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_visaAuthority",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "deployVisaContract",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "borderAuthorityContracts",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "getBorderAuthority",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "getSubContractDetails",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        },
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_uuid",
+          "type": "string"
+        }
+      ],
+      "name": "getVisa",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "subContracts",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "rname",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "pcd",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "password",
+          "type": "string"
+        },
+        {
+          "internalType": "address",
+          "name": "contractAddress",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "subContractUUIDs",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "name": "visaContracts",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+
+  // Initialize ethers by connecting to the network
+  const provider = new ethers.JsonRpcProvider('https://eth-sepolia-public.unifra.io');
+  const privateKey = '2d30cb4d937e240141bb54d14a5621e802b4e9b89a618b53a5b9f59a18c1fddd';
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  console.log('Form values:' ,_token, _rname, _pcd, _password, _uuid);
+  const subContractFactory = new ethers.ContractFactory( GOV_CONTRACT_ABI, GOV_CONTRACT_BYTECODE, signer);
+
+  try {
+    const subContract = await subContractFactory.deploy(
+      govContractAddress, // Replace with the actual address of your gov contract
+      _rname,
+      _pcd,
+      _password,
+      _uuid
+    );
+  
+    // Wait for the deployment to be mined
+    await subContract.deployed();
+  
+    console.log('SubContract deployed at:', subContract.address);
+  } catch (error) {
+    console.error('Error deploying SubContract:', error.message);
+  }
+};
 
 const Sign_up = () => {
   return (
@@ -45,7 +361,7 @@ const Sign_up = () => {
           </div>
 
           <div className="bg-pink-here max-w-7xl pl-10 pr-20 rounded-3xl border-4 border-blue-here">
-            <form className="font-kelly ml-10 mt-5 space-y-2">
+            <form onSubmit={handleSubmit} className="font-kelly ml-10 mt-5 space-y-2">
               {/* Full Name */}
               <label htmlFor="fullname" className="text-3xl mt-8">
                 Full Name
@@ -53,6 +369,7 @@ const Sign_up = () => {
               <br />
               <input
                 type="text"
+                name = "fullname"
                 placeholder="Full Name"
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
@@ -67,6 +384,7 @@ const Sign_up = () => {
               <br />
               <input
                 type="email"
+                name = "email"
                 placeholder="Email"
                 required
                 className="font-normal h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
@@ -81,6 +399,7 @@ const Sign_up = () => {
               <br />
               <input
                 type="text"
+                name ="address"
                 placeholder="Address"
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
@@ -95,6 +414,7 @@ const Sign_up = () => {
               <br />
               <input
                 type="text"
+                name = "aadhar"
                 placeholder="Aadhar No."
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
@@ -109,6 +429,7 @@ const Sign_up = () => {
               <br />
               <input
                 type="password"
+                name = "password"
                 placeholder="Password"
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
@@ -117,7 +438,7 @@ const Sign_up = () => {
 
               {/* Buttons */}
               <br />
-              <button className="rounded-2xl bg-background h-12 w-96 border-4 border-blue-here hover:border-background hover:bg-opacity-40 hover:text-black">
+              <button type="submit" className="rounded-2xl bg-background h-12 w-96 border-4 border-blue-here hover:border-background hover:bg-opacity-40 hover:text-black">
                 <a href="#!">SUBMIT</a>
               </button>
               <br />
