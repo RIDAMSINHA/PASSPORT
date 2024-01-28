d37261f6bd28e008a15f145bbde563f7e22a27eb
RIDAMSINHA <ridamsinha20@gmailcom>
2024-01-28 14:33:50 +0530

    neww commit

--- src/components/User_Login.js
+++ src/components/User_Login.js
@@ -1,18 +1,258 @@
-import React from 'react';
-import '../styles/style.css'; // Import your CSS file
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/style.css"; // Import your CSS file
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const _pcd = event.target.username.value;
+    const _password = event.target.password.value;
+    const GOV_CONTRACT_ADDRESS = "0xf0A5281b6810172467c84248BAafFF01c7F189B4";
+    const GOV_CONTRACT_ABI = [
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_password",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_uuid",
+            "type": "string"
+          }
+        ],
+        "name": "resetPassword",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "contract gov",
+            "name": "_token",
+            "type": "address"
+          },
+          {
+            "internalType": "string",
+            "name": "_rname",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_pcd",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_password",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_uuid",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "inputs": [],
+        "name": "description",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "email",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "contract BorderAuthority",
+            "name": "borderAuthorityContract",
+            "type": "address"
+          }
+        ],
+        "name": "getTravelHistory",
+        "outputs": [
+          {
+            "internalType": "string[]",
+            "name": "",
+            "type": "string[]"
+          },
+          {
+            "internalType": "string[]",
+            "name": "",
+            "type": "string[]"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "contract Visa",
+            "name": "visaContract",
+            "type": "address"
+          }
+        ],
+        "name": "getVisaStatus",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "location",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "_password",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "_pcd",
+            "type": "string"
+          }
+        ],
+        "name": "login",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "pcd",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "rname",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "token",
+        "outputs": [
+          {
+            "internalType": "contract gov",
+            "name": "",
+            "type": "address"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      }
+    ]
+    // Initialize ethers by connecting to the network
+    const provider = new ethers.JsonRpcProvider(
+      "https://eth-sepolia-public.unifra.io"
+    );
+    const privateKey =
+      "2d30cb4d937e240141bb54d14a5621e802b4e9b89a618b53a5b9f59a18c1fddd";
+    const wallet = new ethers.Wallet(privateKey);
+    const signer = wallet.connect(provider);
+  
+    const govContract = new ethers.Contract(
+      GOV_CONTRACT_ADDRESS,
+      GOV_CONTRACT_ABI,
+      signer
+    );
+  
+      try {
+        console.log("Attempting to log in with password:", _password, "and pcd:", _pcd);
+    
+        const loginSuccessful = await govContract.login(_password, _pcd);
+    
+        if (loginSuccessful) {
+            // If the login was successful, redirect the user to the next page
+            console.log("Login successful!");
+            navigate("/gov_portal");
+        } else {
+            // If the login was not successful, show an error message
+            console.error("Invalid credentials. Login failed.");
+            setErrorMessage("Invalid credentials. Please try again.");
+        }
+    } catch (error) {
+        console.error("Error calling login function:", error);
+        setErrorMessage("Transaction failed. Please try again.");
+    }
+    
+  };
+  
   return (
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../styles/style.css" />
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -22,10 +262,7 @@ const Login = () => {
           href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:wght@700&family=Fredoka&family=Koh+Santepheap:wght@300;400;700&family=Roboto+Condensed:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -46,51 +283,60 @@ const Login = () => {
           </div>
 
           <div className="bg-pink-here rounded-3xl border-4 border-blue-here pr-10">
-            <form className="font-kelly ml-10 mt-10 space-y-2">
+            {errorMessage && (
+              <div className="text-red-500">{errorMessage}</div>
+            )}
+            <form
+              onSubmit={handleSubmit}
+              className="font-kelly ml-10 mt-10 space-y-2"
+            >
               {/* Email */}
-              <label htmlFor="email" className="text-3xl">
+              {/* <label htmlFor="email" className="text-3xl">
                 Email
               </label>{' '}
               <br />
               <input
+              name='email'
                 type="email"
                 placeholder="Email"
                 required
                 className="font-normal h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
               />{' '}
               <br />
-              <br />
-
+              <br /> */}
               {/* User Name */}
               <label htmlFor="username" className="text-3xl mt-8">
                 Username
               </label>
               <br />
               <input
+                name="username"
                 type="text"
                 placeholder="Username"
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
-              />{' '}
+              />{" "}
               <br />
               <br />
-
               {/* Password */}
               <label htmlFor="password" className="text-3xl">
                 Password
               </label>
               <br />
               <input
+                name="password"
                 type="password"
                 placeholder="Password"
                 required
                 className="h-10 w-96 px-5 focus:border-blue-here focus:border-4 hover:border-blue-here hover:border-4"
               />
               <br />
-
               {/* Buttons */}
               <br />
-              <button className="rounded-2xl bg-background h-12 w-96 border-4 border-blue-here hover:border-background hover:bg-opacity-40 hover:text-black">
+              <button
+                type="submit"
+                className="rounded-2xl bg-background h-12 w-96 border-4 border-blue-here hover:border-background hover:bg-opacity-40 hover:text-black"
+              >
                 <a href="#!">SUBMIT</a>
               </button>
               <br />
